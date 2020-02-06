1d41f55b821df9687db1272201c6e8c6ed02b8f7
Joram <majoram68@gmail.com>
2020-02-06 09:13:16 -0600

    your turn

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    document.getElementById('content').innerHTML = 'Hello, world!';
+    $.get({
+        url: 'https://lgk9c3wzdi.execute-api.us-east-2.amazonaws.com/dev/users' ,
+        success: function(data) {
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement('div');
+                div.innerHTML = data[i].name.S;
+                div.className = 'user';
+                document.getElementById('content').appendChild(div);
+            }
+        }
+    });
+
+    
+
 });
