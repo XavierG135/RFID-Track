32c5a1bf23d23bebdcb1e4ac62a9b57a55796d8e
Joram <majoram68@gmail.com>
2020-02-10 08:53:26 -0600

    added header

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ $(document).ready(function() {
         url: 'https://lgk9c3wzdi.execute-api.us-east-2.amazonaws.com/dev/users' ,
         success: function(data) {
             var table = document.createElement('table');
+            var trH =  document.createElement('tr');
+            var header = table.createTHead();
+            var thNAME = document.createElement('th');
+            var thGENDER = document.createElement('th');
+            var thAGE = document.createElement('th');
+            header.appendChild(thNAME);
+            header.appendChild(thAGE);
+            header.appendChild(thGENDER);
+            thNAME.innerHTML = 'NAME';
+            thAGE.innerHTML = 'AGE';
+            thGENDER.innerHTML = 'GENDER';
+            thNAME.className = 'thName';
+            thGENDER.className = ' thGender';
+            thAGE.className = 'thAge';
             for (var i = 0; i < data.length; i++) {
                 var tr = document.createElement('tr');
                 var tdName = document.createElement('td');    
@@ -24,7 +38,7 @@ $(document).ready(function() {
                 tdName.className = 'name';
                 tdGender.className = 'gender';
                 tdAge.className = 'age';
-                //div.className = 'user';
+                div.className = 'user';
                 
             }
             document.getElementById('content').appendChild(table);
